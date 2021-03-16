460fa174c88e8f8ca0d444409fbf03922c2b17a4
Shreyans207 <75474973+Shreyans207@users.noreply.github.com>
2021-03-16 16:04:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,24 +7,50 @@ function preload(){
 }
 
 function setup() {
+  createCanvas(displayWidth , displayHeight )
+
+  foodObject = new Feed();
+
+  database = firebase.database();
+  foodStock=database.ref('Food'); 
+  foodStock.on("value", readStock );
+
+  feed=createButton("Feed the dog"); feed.position(700,95); 
+  feed.mousePressed(feedDog); addFood=createButton("Add Food");
+  addFood.position(800,95); addFood.mousePressed(addFoods);
+
   createCanvas(1000,400);
   
   dog=createSprite(800,200,150,150);
   dog.addImage(sadDog);
   dog.scale=0.15;
 
-  foodObject = new Feed();
+  
 
 }
 
 function draw() {
   background(46,139,87);
   foodObject.display();
-  foodObject.hour();
+  foodObject.getFedTime();
+
+  fill(255,255,255);
+        textSize(15);
+        if(lastFed>=12){
+            text("Last Feed : " + lastFed%12 + " PM" , 350,30);
+        }else if(lastFed == 0){
+            text("Last Feed : 12 AM" , 350,30 )
+        }else {
+            text("Last Feed : " + lastFed + " AM" , 350,30 )
+        }
+
   drawSprites();
 }
 
 //function to read food Stock
+function readStock(data){
+   foodS=data.val(); foodObject.updateFoodStock(foodS); 
+  }
 
 
 //function to update food stock and last fed time
@@ -39,9 +65,6 @@ function feedDog(){
 
 
 //function to add food in stock
-function addFood(){
-  foodObject++;
-  database.ref("/").update({
-  food : foodS  
-  })
-}
+function addFoods(){
+   foodS++; database.ref('/').update({ Food:foodS }) 
+  }
